9275c8ae156b080a44d5f9aa0b78a95a886f950c
enrriquemicro <enrriquemicro@gmail.com>
2016-12-21 17:34:37 -0500

    Add files via upload

--- banner.py
+++ banner.py
@@ -3,8 +3,8 @@
 		#QtGui.QMainWindow.__init__(self, parent)
 		#self.publi.clicked.connect(self.publi_clicked)
 	#def publi_clicked(self):
-	def publicidad(self):
+	def publicidad():
 		# Insertar texto en QLabel
-		self.ver.setText("hola")
+		ver.setText("hola")
 		# Insertar archivo desde url
-		self.vista.load(QUrl('https://github.com/enrriquemicro/banner/blob/master/banner2.png?raw=true'))
+		vista.load(QUrl('https://github.com/enrriquemicro/banner/blob/master/banner2.png?raw=true'))
