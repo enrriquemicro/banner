266353f6e6710a6430367f0e4d2c96a0b4e59fcc
enrriquemicro <enrriquemicro@gmail.com>
2016-12-21 16:46:19 -0500

    Add files via upload

--- banner.py
+++ banner.py
@@ -1,7 +1,7 @@
-class MyWindowClass(QtGui.QMainWindow, form_class):
-	def __init__(self, parent=None):
-		QtGui.QMainWindow.__init__(self, parent)
-		self.publi.clicked.connect(self.publi_clicked)
+#class MyWindowClass(QtGui.QMainWindow, form_class):
+	#def __init__(self, parent=None):
+		#QtGui.QMainWindow.__init__(self, parent)
+		#self.publi.clicked.connect(self.publi_clicked)
 	def publi_clicked(self):
 		# Insertar texto en QLabel
 		self.ver.setText("hola")
